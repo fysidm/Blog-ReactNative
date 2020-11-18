c5698d21f6323d9c1c8e87fbd366251b65b4d562
GeorgeWu <fysidm@gmail.com>
2020-11-18 16:27:56 +0800

    Customize form onsubmit

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -8,7 +8,13 @@ const CreateScreen = ({ navigation }) => {
   const { addBlogPost } = useContext(Context);
 
   return (
-    <BlogPostForm />
+    <BlogPostForm
+      onSubmit={(title, content) => {
+        addBlogPost(title, content, () => {
+          navigation.navigate('Index');
+        });
+      }}
+    />
   );
 };
 
