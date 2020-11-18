8735ecdba6b49646c109101f5eb4aa4bc3a9b6a8
GeorgeWu <fysidm@gmail.com>
2020-11-18 15:27:24 +0800

    Add form in CreateScreen

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,14 +1,43 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 
 const CreateScreen = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
   return (
     <View>
-      <Text>Create Screen</Text>
+      <Text style={styles.label}>Enter title: </Text>
+      <TextInput
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+        style={styles.input}
+      />
+      <Text style={styles.label}>Enter content: </Text>
+      <TextInput
+        value={content}
+        onChangeText={(text) => setContent(text)}
+        style={styles.input}
+      />
+      <Button title="Add Blog Post" />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 15,
+    padding: 5,
+    margin: 5
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+    marginLeft: 5
+  }
+});
 
 export default CreateScreen;
