7a214250179a782a3429b18e578ee419f878775e
GeorgeWu <fysidm@gmail.com>
2020-11-18 14:15:32 +0800

    Add delete icon with styles

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { EvilIcons } from '@expo/vector-icons';
 
 import { Context } from '../context/BlogContext';
 
@@ -8,7 +9,6 @@ const IndexScreen = () => {
 
   return (
     <View>
-      <Text>Index Screen</Text>
       <Button
         title="Add Post"
         onPress={addBlogPost}
@@ -18,7 +18,10 @@ const IndexScreen = () => {
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
           return (
-            <Text>{item.title}</Text>
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <EvilIcons style={styles.icon} name="trash" />
+            </View>
           );
         }}
       />
@@ -26,6 +29,21 @@ const IndexScreen = () => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: 'gray'
+  },
+  title: {
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 24
+  }
+});
 
 export default IndexScreen;
